d2fa090008c023dc71d9d5c710867e75894f9b93
jesica.rechy <jesica.rechy@gdl-lap-313.3pillar.corp>
2022-01-29 11:46:34 -0600

    add drivers add data types y operadores

---
+++ SeleniumCursoEnero/src/test/java/com/java/DataTypes.java
@@ -0,0 +1,48 @@
+package com.java;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       //comentarios
+		
+		//datos primitivos
+		byte var = 1; //Numerico entero
+		short var2 = 1; // Numerico entero
+		int var3 = 1; // Numerico entero -Automation
+		long var4 = 1L; // Numerico muy largo entero -Automation
+		float var5 = 1.5f; // numerico decimal
+		double var6 = 2.4585; // numerico real (decimales) -automation
+		char var7 = 'a'; //caracter
+		boolean var8 = true; // Automation- tiene dos valores TRUE / FALSE
+		
+		//Declarar variables
+		int x;
+		x = 10000;
+		
+		int y = 10;
+		
+		//Char symbol example
+		char symbol = '\u00A9';
+		
+		// Cadena Char
+		String message = "Hello World";
+		
+		//Imprimir en consola
+		System.out.println(symbol);
+		System.out.println(message);
+		
+		//concatenacion se representa con el signo de +
+		System.out.println(message + symbol);
+		int numero = 1;
+		int numero2 = 2;
+		String nombre = "Jesica";
+		System.out.println(nombre + numero);
+		System.out.println(numero2 + numero);
+		
+		
+		
+		
+	}
+
+}
